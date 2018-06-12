e683f124401b0c07840b49ba365779f4c7b3d159
Ben-Wah Kuang <ben-wah.kuang@bell.ca>
2018-06-12 18:08:05 -0400

    Set build folder structure in build and run scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
+mkdir -p $DIR/tmp/;
+mkdir -p $DIR/build/classes/main/;
+mkdir -p $DIR/build/classes/test/;
+
 FOLDER_MAIN_CLASSES=$DIR/build/classes/main/
 FOLDER_TEST_CLASSES=$DIR/build/classes/test/
 
