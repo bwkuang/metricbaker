2f33f6e70d654fefe188a788285de4e0f21f56b6
Ben-Wah Kuang <ben-wah.kuang@bell.ca>
2018-07-08 14:47:39 -0400

    Remove dead code

--- src/main/java/metricbaker/Ingredient.java
+++ src/main/java/metricbaker/Ingredient.java
@@ -11,35 +11,14 @@ public class Ingredient {
     private String name;
     private float amount;
     private Quantity quantity;
-    private UnitOfVolume unitOfVolume = UnitOfVolume.none;
-    private UnitOfMass unitOfMass = UnitOfMass.none;
 
     private Ingredient(String name, Quantity quantity){
         this.name = name;
         this.quantity = quantity;
     }
 
-    private Ingredient(String name, float amount, UnitOfVolume unitOfVolume){
-        this.name = name;
-        this.amount = amount;
-        this.unitOfVolume = unitOfVolume;
-    }
-
-    private Ingredient(String name, float amount, UnitOfMass unitOfMass){
-        this.name = name;
-        this.amount = amount;
-        this.unitOfMass = unitOfMass;
-    }
-
     public int getWeight(){
-        float volume = unitOfVolume.getVolumeInMl();
-        float density = IngredientDensity.getDensityByIngredient(name);
-
-        float unitMassInGram = unitOfMass.getMassInGrams();
-
         return quantity.getWeight(IngredientDensity.getDensityByIngredient(this.name));
-
-        // return  Math.max(Math.round(volume * density), Math.round(amount * unitMassInGram));
     }
 
     public static Ingredient breadFlour(Quantity quantity){
